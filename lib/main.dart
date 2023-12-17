9aec3490d9b803977714aed272e93a8455a532a1
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-17 16:46:55 +0100

    Modified  lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_animate/flutter_animate.dart';
 import 'package:speak/home.dart';
 import 'package:speak/utils/methods.dart';
 import 'package:speak/wait.dart';
+import 'package:speak/wrong.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,9 +21,13 @@ class Main extends StatelessWidget {
       home: FutureBuilder<bool>(
         future: load(),
         builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-          if(snapshot.hasData){return const Home();}
-          else if(snapshot.connectionState == ConnectionState.waiting){return const Wait();}
-          else {return }
+          if (snapshot.hasData) {
+            return const Home();
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Wait();
+          } else {
+            return Wrong(errorMessage: snapshot.error.toString());
+          }
         },
       ),
       theme: ThemeData.dark(),
