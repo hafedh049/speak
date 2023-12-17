2e1c2bff0aa59c82b9301563b3d9d154ec9a45ef
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-17 16:31:40 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -62,7 +62,7 @@ class _HomeState extends State<Home> {
             SizedBox(
               child: PageView.builder(
                 itemCount: _translations.length,
-                itemBuilder: (context, index) {},
+                itemBuilder: (BuildContext context, int index) {},
               ),
             ),
           ],
