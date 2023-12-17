dfa66b27a574721d70412e62180ca3312063532f
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-17 16:31:16 +0100

    ModifiedM lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -61,6 +61,7 @@ class _HomeState extends State<Home> {
             const SizedBox(height: 10),
             SizedBox(
               child: PageView.builder(
+                itemCount: _translations.length,
                 itemBuilder: (context, index) {},
               ),
             ),
