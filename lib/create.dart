619ac681fc730f6e7f0150fa44046d1e563ed804
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-17 18:22:21 +0100

    Modified  lib/create.dart

--- lib/create.dart
+++ lib/create.dart
@@ -33,7 +33,7 @@ class _CreateState extends State<Create> {
                 ),
                 const SizedBox(width: 10),
                 Container(
-                  decoration: const BoxDecoration(shape: BoxShape.circle, color: orange),
+                  decoration: const BoxDecoration(color: orange),
                   child: IconButton(onPressed: () async {}, icon: const Icon(Bootstrap.plus, size: 20, color: white)),
                 ),
               ],
