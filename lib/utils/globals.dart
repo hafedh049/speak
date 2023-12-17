d092df7f64ab11e857171b1eb211955b592170c7
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-17 14:16:02 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const Color backgroundColor = Color.fromARGB(255, 33, 37, 43);
 const Color secondaryColor = Color.fromARGB(255, 40, 44, 52);
+const Color gray = Color.fromARGB(255, 51, 56, 66);
