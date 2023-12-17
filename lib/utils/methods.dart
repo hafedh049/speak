c6b7ba84257ad991bbc1f35502de04f2107502bf
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-17 14:51:56 +0100

    Modified  lib/utils/methods.dart

--- lib/utils/methods.dart
+++ lib/utils/methods.dart
@@ -1,10 +1,11 @@
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:speak/utils/globals.dart';
 
 Future<bool> load() async {
   try {
     Hive.init((await getApplicationDocumentsDirectory()).path);
-
+    translationsBox = Hive.openBox(name);
     return true;
   } catch (e) {
     return false;
