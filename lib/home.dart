58ae825748975232e1ffb9510bc99299b619cc31
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-17 16:51:17 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -10,7 +10,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final Map<String, List<Map<String, dynamic>>> _translations = <String, List<Map<String, dynamic>>>{};
+  final Map<String, Map<String, dynamic>> _translations = <String, Map<String, dynamic>>{};
   final PageController _pageController = PageController();
 
   @override
