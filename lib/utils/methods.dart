c24a0ecdaa884b4deac1e6da4bed71326d8a8b0e
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-17 14:38:50 +0100

    ModifiedM lib/utils/methods.dart

--- lib/utils/methods.dart
+++ lib/utils/methods.dart
@@ -1,9 +1,10 @@
-Map<String, dynamic> getMonthDetails(int monthNumber) {
+import 'package:speak/utils/globals.dart';
 
+Map<String, dynamic> getMonthDetails(int monthNumber) {
   final now = DateTime.now();
   final currentMonth = DateTime(now.year, monthNumber + 1, 1);
 
-  final monthName = months[currentMonth.month - 1].toUpperCase();
+  final monthName = months[currentMonth.month - 1];
   final numberOfDays = DateTime(now.year, monthNumber + 2, 0).day;
   final currentDay = now.day;
 
