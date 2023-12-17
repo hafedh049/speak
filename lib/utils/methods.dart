3714fc8dc1dcede4706183b5c92e13839dcf3b0c
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-17 17:22:38 +0100

    Modified  lib/utils/methods.dart

--- lib/utils/methods.dart
+++ lib/utils/methods.dart
@@ -13,6 +13,7 @@ Future<bool> load() async {
     } else if (translationsBox!.get("translations").isEmpty) {
       translationsBox!.put("translations", <String, Map<String, dynamic>>{"17-12-2023": <String, dynamic>{}});
     }
+
     translationsBox!.put("translations", translationsBox!.get("translations")..addAll({"${Random().nextInt(29) + 1}-12-2023": {}}));
     return true;
   } catch (e) {
