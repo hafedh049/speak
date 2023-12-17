ee9b17528f66323861282d052aee13c26576923c
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-17 15:11:44 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -41,7 +41,7 @@ class _HomeState extends State<Home> {
                   ),
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[],
+                    children: <Widget>[Text()],
                   ),
                 );
               },
