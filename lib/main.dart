37892d71328fb16d3756a01ea059e168397f4da5
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-17 16:42:39 +0100

    Modified  lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:speak/home.dart';
+import 'package:speak/utils/methods.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,7 +16,11 @@ class Main extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const Home(),
+      home: FutureBuilder<bool>(
+          future: load(),
+          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+            return const Home();
+          }),
       theme: ThemeData.dark(),
     );
   }
