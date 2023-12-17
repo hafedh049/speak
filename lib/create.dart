38ecaf7a49a615786883fc69b6e98073433bb0d0
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-17 18:19:38 +0100

    Modified  lib/create.dart

--- lib/create.dart
+++ lib/create.dart
@@ -22,7 +22,7 @@ class _CreateState extends State<Create> {
               children: <Widget>[
                 Container(
                   decoration: const BoxDecoration(shape: BoxShape.circle, color: orange),
-                  child: IconButton(onPressed: () async {}, icon: const Icon(Bootstrap.plus, size: 20, color: white)),
+                  child: IconButton(onPressed: () async {}, icon: const Icon(Bootstrap.chevron_bar_left, size: 20, color: white)),
                 ),
                 const SizedBox(width: 10),
                 const Text("SPEAK", style: TextStyle(color: white, fontSize: 20, letterSpacing: 2)),
