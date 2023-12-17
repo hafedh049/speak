942ab79aa9d62a24136d85aeb0d5e4639c0336f6
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-17 14:47:54 +0100

    Modified  lib/utils/globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 const Color backgroundColor = Color.fromARGB(255, 33, 37, 43);
 const Color secondaryColor = Color.fromARGB(255, 40, 44, 52);
@@ -9,3 +10,5 @@ const Color orange = Color.fromARGB(255, 254, 131, 104);
 const Color blue = Color.fromARGB(255, 88, 129, 234);
 
 final List<String> months = <String>['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
+Box? translationsBox;
