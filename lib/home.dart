1559c3e3485e5705646eac6188a7976f4a8a0eff
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-17 18:38:43 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -82,6 +82,7 @@ class _HomeState extends State<Home> {
                                       ),
                               ),
                               const SizedBox(height: 5),
+                              AnimatedContainer(duration: duration),
                             ],
                           ),
                         );
