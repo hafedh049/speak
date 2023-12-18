53fef8d2026f64b7ff029b09deab38ce071ca7cc
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-18 14:46:04 +0100

    Modified  lib/create.dart

--- lib/create.dart
+++ lib/create.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -127,7 +129,7 @@ class _CreateState extends State<Create> {
                                   if (_from != languageMap.keys.elementAt(index)) {
                                     _fromKey.currentState!.setState(() => _from = languageMap.keys.elementAt(index));
                                     if (_to == _from) {
-                                      _toKey.currentState!.setState(() => _from = languageMap.keys.elementAt(index));
+                                      _toKey.currentState!.setState(() => _to = languageMap.keys.elementAt(Random().nextInt(languageMap.length)));
                                     }
                                   }
                                   Navigator.pop(context);
