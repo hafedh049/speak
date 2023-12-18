40baf9bb27b6a6185dff1b71d97b7df9c113adb3
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-18 09:18:05 +0100

    Modified  lib/create.dart

--- lib/create.dart
+++ lib/create.dart
@@ -19,7 +19,7 @@ class _CreateState extends State<Create> {
   final GlobalKey<State> _inputClipboardKey = GlobalKey<State>();
   final GlobalKey<State> _outputSpeakKey = GlobalKey<State>();
   final GlobalKey<State> _outputClipboardKey = GlobalKey<State>();
-  final GlobalKey<State> _outputClipboardKey = GlobalKey<State>();
+  final GlobalKey<State> _outputKey = GlobalKey<State>();
 
   @override
   void dispose() {
